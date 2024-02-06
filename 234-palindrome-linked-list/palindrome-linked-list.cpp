b4c3c85f5acf7989caadd3877bb049ae62a6eb57
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-02-07 01:44:21 +0530

    Time: 164 ms (79.01%) | Memory: 116.6 MB (70.16%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -34,7 +34,82 @@ public:
 
         return true;
     }
+
+    ListNode* middle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next!=NULL && fast->next->next!=NULL) {
+            slow = slow->next;
+            if(fast->next->next!=NULL){
+                fast = fast->next->next;
+            }
+        }
+
+        if(fast->next!=NULL){
+            return slow->next;
+        }
+
+        return slow;
+    }
+
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr!=NULL){
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    bool compare(ListNode* head1, ListNode* head2){
+        while(head2!=NULL){
+            // cout<<"head1:"<<head1->val<< " | head2:"<<head2->val;
+            if(head1->val!=head2->val){
+                return false;
+            }
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+
+
+        return true;
+    }
+
+    bool optimal(ListNode* head) {
+        // find middle
+        ListNode* temp = head;
+        ListNode* mid = middle(temp);
+        // cout<<"middle"<<mid->val;
+
+        ListNode* head2 = reverse(mid);
+
+        // cout<<"middle: "<<mid->val;
+        ListNode* head1 = head;
+
+        // while(head1!=NULL){
+        //     cout<<head1->val<<" ";
+        //     head1 = head1->next;
+        // }
+
+        // cout<<"ttt";
+
+        //   while(head2!=NULL){
+        //     cout<<head2->val<<" ";
+        //     head2 = head2->next;
+        // }
+
+        return compare(head1,head2);
+
+        // return false;
+    }
     bool isPalindrome(ListNode* head) {
-        return brute(head);
+        // return brute(head);
+        return optimal(head);
     }
 };
