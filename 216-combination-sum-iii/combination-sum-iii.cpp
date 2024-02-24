73a48b8b7ac8216171251fa21b323dca5820b673
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-02-25 02:32:21 +0530

    Time: 3 ms (47.77%) | Memory: 7.7 MB (47.09%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+    void generate(int num, vector<int> &curr,int digits, int sum, vector<vector<int>> &ans){
+
+        if(digits == 0){
+            if(sum == 0){
+                ans.push_back(curr);
+            }
+
+            return;
+        }
+
+        // cout<<"num:"<<num;
+
+        for(int i = num+1; i<10; i++){
+
+            if(sum < i){
+                continue;
+            }
+            //cout<<"recursion:"<<i;
+
+            curr.push_back(i);
+            generate(i,curr,digits-1,sum-i,ans);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+
+        int minSum = 0;
+        for(int i=1; i<=k; i++){
+            minSum += i;
+        }
+
+        if (n < minSum) {
+            return ans;
+        }
+
+        vector<int> curr;
+
+        generate(0,curr,k,n,ans);
+
+        return ans;
+        
+    }
+};
