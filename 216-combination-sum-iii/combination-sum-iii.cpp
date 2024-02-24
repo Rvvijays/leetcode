215b4d21eab429d60c75f50ec87163783ff2f667
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-02-25 02:32:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (50.52%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -11,14 +11,12 @@ public:
             return;
         }
 
-        // cout<<"num:"<<num;
 
         for(int i = num+1; i<10; i++){
 
             if(sum < i){
                 continue;
             }
-            //cout<<"recursion:"<<i;
 
             curr.push_back(i);
             generate(i,curr,digits-1,sum-i,ans);
@@ -28,14 +26,14 @@ public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
 
-        int minSum = 0;
-        for(int i=1; i<=k; i++){
-            minSum += i;
-        }
+        // int minSum = 0;
+        // for(int i=1; i<=k; i++){
+        //     minSum += i;
+        // }
 
-        if (n < minSum) {
-            return ans;
-        }
+        // if (n < minSum) {
+        //     return ans;
+        // }
 
         vector<int> curr;
 
