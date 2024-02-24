0f4d98ba747dd9eb52ecf424614913b2a4cce537
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-02-25 00:07:56 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (63.29%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -8,34 +8,54 @@ public:
             return;
         }
 
+        curr.push_back(arr[index]);
+        generate(index+1,curr,arr,ans);
+        curr.pop_back();
+        generate(index+1,curr,arr,ans);
+    }
+
+    void optimal(int index, vector<int> &curr, vector<int> &arr, vector<vector<int>> &ans){
+
+        ans.push_back(curr);
 
 
-        // for(int i=index; i<arr.size(); i++){
-        //     if(i>index && arr[i] == arr[i-1]){
-        //         continue;
-        //     }
+        for(int i=index; i<arr.size(); i++){
+            if(i>index && arr[i]==arr[i-1]){
+                continue;
+            }
 
-            curr.push_back(arr[index]);
-            generate(index+1,curr,arr,ans);
+            curr.push_back(arr[i]);
+            optimal(i+1,curr,arr,ans);
             curr.pop_back();
-            generate(index+1,curr,arr,ans);
-        // }
+        }
+        
+
+
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
 
-        set<vector<int>> ans;
-        vector<int> curr;
+        // set<vector<int>> ans;
+        // vector<int> curr;
 
         sort(nums.begin(),nums.end());
 
-        generate(0,curr,nums,ans);
+        // generate(0,curr,nums,ans);
 
-        vector<vector<int>> anss;
-        for (auto i : ans) {
-            anss.push_back(i);
-        }
+        // vector<vector<int>> anss;
+        // for (auto i : ans) {
+        //     anss.push_back(i);
+        // }
 
-        return anss;
+        // return anss;
+
+
+        vector<vector<int>> ans;
+        vector<int> curr;
+
+        optimal(0,curr,nums,ans);
+        return ans;
+
+
         
     }
 };
