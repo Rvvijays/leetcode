c07c7d147465fdcfd725b5d275a2dfa4458dcaf9
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-02-25 12:53:19 +0530

    Time: 4 ms (14.15%) | Memory: 8.4 MB (12.26%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -7,7 +7,6 @@ public:
             return {};
         }
 
-        
         const vector<string> pad = {
             "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
         };
@@ -30,15 +29,42 @@ public:
         }
 
         return ans;
-        
-
-       
-
 
     }
+
+    void generate(string prefix,int index,string digits,vector<string> &keys,vector<string> &ans){
+
+        if(index >= digits.length()){
+            ans.push_back(prefix);
+            return;
+        }
+
+
+        string letters = keys[digits[index] - '0'];
+        // cout<<"letters:"<<letters;
+
+        for(auto letter : letters) {
+            generate(prefix+letter,index+1,digits,keys,ans);
+        }
+
+    }
+
+
     vector<string> letterCombinations(string digits) {
 
-        return brute(digits);
+        // return brute(digits);
+
+        if(digits.length()==0){
+            return {};
+        }
+
+        vector<string> ans;
+
+        vector<string> keys = {
+            "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
+        };
+        generate("",0,digits,keys,ans);
+        return ans;
 
         
     }
