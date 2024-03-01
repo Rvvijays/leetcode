25a3db3e95d0ef5c408a2f66f8565c14dd2f5067
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-03-01 17:12:49 +0530

    Time: 7 ms (11.93%) | Memory: 11.2 MB (29.69%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -26,9 +26,47 @@ public:
         return -1;
     }
 
+    int optimal(vector<int> &nums) {
+         int n = nums.size();
+        if(n == 1){
+            return 0;
+        }
+
+        if(nums[0] > nums[1]){
+            return 0;
+        }
+
+        if(nums[n-1]> nums[n-2]){
+            return n-1;
+        }
+
+        int low = 1;
+        int high = n-2;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+
+            if(nums[mid]> nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+
+            if(nums[mid-1] < nums[mid]  && nums[mid] < nums[mid+1]){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+
+
+    }
+
     int findPeakElement(vector<int>& nums) {
 
-        return brute(nums);
+        // return brute(nums);
+
+        return optimal(nums);
         
     }
 };
