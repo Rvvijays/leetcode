22397c4370727df091d3494cb72a1a9730fc919a
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-03-27 23:31:56 +0530

    Time: 98 ms (95.22%) | Memory: 52 MB (69.17%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+
+    bool isCycle(int node,vector<vector<int>>& graph,vector<int> &vis, vector<int> &pathVis,vector<int> &check){
+        vis[node] = 1;
+        pathVis[node] = 1;
+        check[node]= 0;
+
+        for(auto it: graph[node]){
+            if(vis[it]==0){
+
+                
+                
+                if (isCycle(it,graph,vis,pathVis,check)){
+                    check[node] = 0;
+                    return true;
+                }
+            }else if(vis[it]!=0 && pathVis[it]!=0){
+                check[node] = 0;
+                return true;
+            }
+        }
+
+        pathVis[node] = 0;
+        check[node] = 1;
+        return false;
+
+
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+
+        vector<int> vis(graph.size(),0);
+        vector<int> pathVis(graph.size(),0);
+        vector<int> check(graph.size(),0);
+
+        for(int i=0; i<graph.size(); i++){
+            if(vis[i]==0){
+                isCycle(i,graph,vis,pathVis,check);
+            }
+        }
+
+        vector<int> ans;
+        for(int i=0; i<graph.size(); i++){
+            if(check[i]==1){
+                ans.push_back(i);
+            }
+
+        }
+
+        return ans;
+
+    }
+};
