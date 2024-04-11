bc9146dee97a3d5cb5a58c5dcc3afbc948588cdf
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-11 23:17:23 +0530

    Time: 2 ms (41.11%) | Memory: 9.4 MB (26.75%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+
+    int recursion(int start, int index, vector<int> &nums) {
+
+        if(index == start){
+            return nums[index];
+        }
+
+        if(index < start ){
+            return 0;
+        }
+        int take = nums[index] + recursion(start, index - 2,nums);
+       
+        int notTake = 0 + recursion(start,index - 1,nums);
+
+        return max(take, notTake);
+    }
+
+    int memorization(int start, int index, vector<int> &nums,vector<int> &dp) {
+
+        if(index == start){
+            return nums[index];
+        }
+
+        if(index < start ){
+            return 0;
+        }
+
+        if(dp[index]!=-1){
+            return dp[index];
+        }
+        int take = nums[index] + memorization(start, index - 2,nums,dp);
+       
+        int notTake = 0 + memorization(start,index - 1,nums,dp);
+
+        return dp[index] = max(take, notTake);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n<2){
+            return nums[0];
+        }
+
+    //    if(n<1){
+
+    //    }
+
+
+        // return max(recursion(0,n-2,nums),recursion(1,n-1,nums));
+
+        vector<int> dp1(n+1,-1);
+        vector<int> dp2(n+1,-1);
+        return max(memorization(0,n-2,nums,dp1),memorization(1,n-1,nums,dp2));
+
+    }
+};
