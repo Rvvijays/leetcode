67b3404c355b02b51705b98f841db0edd334d7b9
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-11 23:28:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (46.30%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -37,23 +37,38 @@ public:
         return dp[index] = max(take, notTake);
     }
 
+    int tabulation(int start, int end, vector<int> &nums){
+        
+        vector<int> dp(end+1,0);
+        dp[start] = nums[start];
+
+        for(int i=start+1; i<=end; i++){
+            int nonTake = dp[i-1];
+            int take = nums[i];
+            if(i>1){
+                take += dp[i-2];
+            }
+
+            dp[i] = max(take,nonTake);
+        }
+
+        return dp[end];
+    }
+
     int rob(vector<int>& nums) {
         int n = nums.size();
 
         if(n<2){
             return nums[0];
         }
-
-    //    if(n<1){
-
-    //    }
-
-
         // return max(recursion(0,n-2,nums),recursion(1,n-1,nums));
 
-        vector<int> dp1(n+1,-1);
-        vector<int> dp2(n+1,-1);
-        return max(memorization(0,n-2,nums,dp1),memorization(1,n-1,nums,dp2));
+        // vector<int> dp1(n+1,-1);
+        // vector<int> dp2(n+1,-1);
+        // return max(memorization(0,n-2,nums,dp1),memorization(1,n-1,nums,dp2));
 
+
+
+        return max(tabulation(0,n-2,nums),tabulation(1,n-1,nums));
     }
 };
