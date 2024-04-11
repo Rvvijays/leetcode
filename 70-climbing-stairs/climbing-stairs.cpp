a5427109bbe051c34c916813072361e33db62c2c
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-11 08:31:26 +0530

    Time: 3 ms (28.38%) | Memory: 7.3 MB (49.29%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -63,13 +63,29 @@ public:
         // return recursion2(n);
         // return memoization2(n,dp);
 
-        dp[0] = dp[1] = 1;
+        // dp[0] = dp[1] = 1;
 
+        // for(int i=2; i<=n; i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+
+        // return dp[n];
+
+
+        // space optimizing
+        int prev2 = 1;
+        int prev1 = 1;
+
+        int curr = 0;
         for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return dp[n];
+        return prev1;
+
+
         
     }
 };
