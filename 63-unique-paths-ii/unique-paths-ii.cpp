41d47053acb1958dcfa5074b404a02bed4607e47
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-13 01:57:40 +0530

    Time: 5 ms (35.72%) | Memory: 10.1 MB (78.24%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -45,40 +45,74 @@ public:
 
         vector<vector<int>> dp(n, vector<int>(m, 0));
 
-        for (int i = 0; i <n; i++) {
-            for (int j = 0; j <m; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
 
                 // if (grid[i][j] == 0) {
-                    if (i == 0 && j == 0) {
-                        dp[i][j] = 1;
-                    } else {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else {
 
-                        if (i > 0 && grid[i-1][j]==0) {
-                            dp[i][j] += dp[i - 1][j];
-                        }
-
-                        if (j > 0 && grid[i][j-1]==0) {
-                            dp[i][j] += dp[i][j - 1];
-                        }
+                    if (i > 0 && grid[i - 1][j] == 0) {
+                        dp[i][j] += dp[i - 1][j];
                     }
+
+                    if (j > 0 && grid[i][j - 1] == 0) {
+                        dp[i][j] += dp[i][j - 1];
+                    }
+                }
                 // }
             }
         }
 
-        return dp[n-1][m-1];
+        return dp[n - 1][m - 1];
     }
+
+    int spaceoptimization(int n, int m, vector<vector<int>>& grid) {
+
+        // vector<vector<int>> dp(n, vector<int>(m, 0));
+
+        vector<int> prev(m,0);
+
+        for (int i = 0; i < n; i++) {
+
+            vector<int> cur(m,0);
+            for (int j = 0; j < m; j++) {
+
+                // if (grid[i][j] == 0) {
+                if (i == 0 && j == 0) {
+                    // dp[i][j] = 1;
+                    cur[0] = 1;
+                } else {
+
+                    if (i > 0 && grid[i - 1][j] == 0) {
+                        cur[j] += prev[j];
+                    }
+
+                    if (j > 0 && grid[i][j - 1] == 0) {
+                        cur[j] += cur[j - 1];
+                    }
+                }
+                // }
+            }
+            prev = cur;
+        }
+
+        return prev[m - 1];
+    }
+
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
- int n = grid.size();
+        int n = grid.size();
         int m = grid[0].size();
-        if (grid[0][0] == 1 || grid[n-1][m-1]==1) {
+        if (grid[0][0] == 1 || grid[n - 1][m - 1] == 1) {
             return 0;
         }
-       
 
         // return recursion(n-1,m-1,grid);
         // vector<vector<int>> dp(n,vector<int>(m,-1));
         // return memorization(n-1,m-1,grid,dp);
 
-        return tabulation(n, m, grid);
+        // return tabulation(n, m, grid);
+        return spaceoptimization(n, m, grid);
     }
 };
