35907e3974ed7c75db9f005d43f43ee869b2ccf8
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-13 15:54:53 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (26.48%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -31,9 +31,6 @@ public:
             return dp[i][j];
         }
 
-
-
-
         int top = memorization(i-1,j,triangle,dp);
         int left = memorization(i-1,j-1,triangle,dp);
 
@@ -41,17 +38,71 @@ public:
     }
 
 
+    int recursion2(int i, int j,int n, vector<vector<int>>& triangle){
+
+        if(i <0 || j <0 || j>i){
+            return 1e5;
+        }
+
+        if(i==n-1) {
+            return triangle[i][j];
+        }
+
+        int bottom = recursion2(i+1,j,n,triangle);
+        int bottomright = recursion2(i+1,j+1,n,triangle);
+
+        return min(bottom,bottomright) + triangle[i][j];
+        
+
+    }
+
+    int memorization2(int i, int j,int n, vector<vector<int>>& triangle, vector<vector<int>>&  dp){
+
+        if(i <0 || j <0 || j>i){
+            return 1e5;
+        }
+
+        if(i==n-1) {
+            return triangle[i][j];
+        }
+
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        int bottom = memorization2(i+1,j,n,triangle,dp);
+        int bottomright = memorization2(i+1,j+1,n,triangle,dp);
+
+        return dp[i][j] =  min(bottom,bottomright) + triangle[i][j];
+        
+
+    }
+   
+
+
     int minimumTotal(vector<vector<int>>& triangle) {
 
         int n = triangle.size();
-        int mini = 1e5;
-        for(int i=0; i<n; i++){
+        // int mini = 1e5;
+        // for(int i=0; i<n; i++){
             // mini = min(mini, recursion(n-1,i,triangle));
-            vector<vector<int>> dp(n,vector<int>(n,-1));
-            mini = min(mini, memorization(n-1,i,triangle,dp));
-        }
+            // vector<vector<int>> dp(n,vector<int>(n,-1));
+            // mini = min(mini, memorization(n-1,i,triangle,dp));
 
-        return  mini;
+        // }
+        // return  mini;
+
+        // we know source if fixed, destination point can be any thing in the row.
+        // so we start from starting. means. 0,0
+        // return recursion2(0,0,n,triangle);
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        return memorization2(0,0,n,triangle,dp);
+
+
+
+       
+
+        
         
     }
 };
