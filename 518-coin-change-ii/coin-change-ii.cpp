2883ee29e07f8a654385aa6fa52b752ebab32780
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-20 15:42:05 +0530

    Time: 10 ms (55.54%) | Memory: 19.6 MB (58.46%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -21,20 +21,12 @@ public:
     int memorization(int index, int target, vector<int> &coins,vector<vector<int>> &dp){
 
         if(index == 0){
-
             if(target % coins[index] ==0){
                 return 1;
             }
-            // if(coins[index]== target){
-            //     return 1;
-            // }
-
             return 0;
         }
 
-        // if(index<0){
-        //     return 0;
-        // }
 
         if(dp[index][target]!=-1){
             return dp[index][target];
@@ -49,15 +41,51 @@ public:
         return dp[index][target] = take + notTake;
 
     }
+
+    int tabulation(int n, int amount, vector<int> &coins){
+
+        vector<vector<int>> dp(n, vector<int>(amount+1,0));
+
+        for(int i=0; i<=amount; i++){
+            if(i % coins[0] == 0){
+                dp[0][i] = 1;
+            }
+        }
+
+        for(int index = 1; index<n; index++){
+            for(int target = 0; target <= amount; target++){
+                int notTake = dp[index-1][target];
+                int take = 0;
+                if(target >= coins[index]){
+                    take = dp[index][target - coins[index]];
+                }
+                 dp[index][target] = take + notTake;
+            }
+        }
+
+        return dp[n-1][amount];
+
+
+       
+
+    }
+
+
+
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
 
 
         // return recursion(n-1,amount,coins);
 
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
 
-        return memorization(n-1,amount,coins,dp);
+        // return memorization(n-1,amount,coins,dp);
+
+
+        return tabulation(n,amount,coins);
+
+
 
         
     }
