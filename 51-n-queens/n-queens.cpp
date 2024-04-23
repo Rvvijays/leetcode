9bed485f4a8f911362a48b75dac7a2d898808994
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-23 13:28:06 +0530

    Time: 4 ms (63.24%) | Memory: 9.2 MB (81.71%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,75 +1,121 @@
 class Solution {
 public:
 
-    bool isPossible(int i,int j, vector<string> &curr){
+    bool isValid(int x,int y, int n, vector<string> &curr){
+
+        // for(int i=0; i<n; i++){
+        // cout<<curr[i]<<endl; 
+        // }
+
+        // cout<<"checking x:"<<x<<" y:"<<y<<endl;
+
+        int i = x;
+        int j = y;
+
+        for(int i=0; i<n; i++){
+            if(curr[i][y] == 'Q'){
+                return false;
+            }
+
+            if(curr[x][i]=='Q'){
+                return false;
+            }
+        }
+
+         i = x;
+         j = y;
+
+        while(i>=0 && j>=0){
+            if(curr[i][j]=='Q'){
+                return false;
+            }
+
+            i--;
+            j--;
+        }
+
+
+
+         i = x;
+         j = y;
+
+        while(i>=0 && j<n){
+            if(curr[i][j]=='Q'){
+                return false;
+            }
+
+            i--;
+            j++;
+        }
+
+
+         i = x;
+         j = y;
+
+        while(j>=0 && i<n){
+            if(curr[i][j]=='Q'){
+                return false;
+            }
+
+            j--;
+            i++;
+        }
+            i = x;
+         j = y;
+        while(j<n && i <0){
+            if(curr[i][j]=='Q'){
+                return false;
+            }
+
+            i++;
+            j++;
+        }
+
         
-        // left top
-
-        int x = i;
-        int y = j;
-        while(x >= 0 && y>=0){
-            if(curr[x][y] == 'Q'){
-                return false;
-            }
-
-            x--;
-            y--;
-        }
-
-        x = i;
-        y = j;
-        while(x<curr.size() && y>=0){
-            if(curr[x][y] =='Q'){
-                return false;
-            }
-            x++;
-            y--;
-        }
- x = i;
-        y = j;
-        while(y>=0){
-            if(curr[x][y] == 'Q'){
-                return false;
-            }
-
-            y--;
-        }
-
         return true;
+
+        // verticle
+
+        // horizontal
+        // diagonal
+
+
     }
 
-    void generate(int col, vector<string> &curr, vector<vector<string>> &ans, int n){
+    void generate(int row, int n, vector<string> &curr, vector<vector<string>> &ans){
 
-       
 
-        if(col == n) {
+        if(row == n){
+            // cout<<"added to ans"<<endl;
             ans.push_back(curr);
             return;
         }
+        
+        // int dx[4] = {0,0,1,-1};
+        // int dy[4] = {1,-1,0,0};
 
         for(int i=0; i<n; i++){
-            if(isPossible(i,col,curr)){
-                curr[i][col] = 'Q';
-                generate(col + 1, curr,ans,n);
-                curr[i][col] = '.';
+
+            if(isValid(row,i,n,curr)){
+                curr[row][i] = 'Q';
+               generate(row+1,n,curr,ans);
+                curr[row][i] = '.';
             }
         }
-
     }
     vector<vector<string>> solveNQueens(int n) {
-
         vector<vector<string>> ans;
-
-        vector<string> board(n);
-        string s(n,'.');
-        for(int i=0; i<n; i++){
-            board[i] = s;
-        }
         
+        string s = "";
+        for(int i=0; i<n; i++){
+            s += ".";
+        }
 
-        generate(0,board,ans,n);
+        
+        vector<string> curr(n,s);
+
+        generate(0,n,curr,ans);
 
         return ans;
-        
     }
 };
