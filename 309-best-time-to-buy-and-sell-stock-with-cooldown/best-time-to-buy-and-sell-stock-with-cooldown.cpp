dd2c5f6e54db1196e21afa8fcd9d22a421dcbaba
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-25 22:32:49 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (6.39%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -36,13 +36,55 @@ public:
         return dp[index][buy][cooldown] = profit;
 
     }
+
+    int tabulation(int n, vector<int> &prices){
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(3,vector<int>(3,0)));
+
+        for(int index=n-1; index>=0; index--){
+            for(int buy=0; buy<2; buy++){
+                for(int cooldown=0; cooldown<2; cooldown++){
+
+                    int profit = 0;
+
+                    if(buy == 0){
+                        //can buy
+
+                        if(cooldown == 0){
+                            profit = max(dp[index+1][1][0] - prices[index], dp[index+1][0][0]);
+                        }else{
+                            profit = dp[index+1][0][0];
+                        }
+                        
+
+                    }else{
+
+                    
+                            profit = max(dp[index+1][0][1] + prices[index],dp[index+1][1][0]);
+
+                        
+
+
+                    }
+
+                     dp[index][buy][cooldown] = profit;
+
+                }
+            }
+        }
+
+        return dp[0][0][0];
+
+
+    }
     int maxProfit(vector<int>& prices) {
 
         int n = prices.size();
 
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(2,-1)));
+        // vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(2,-1)));
 
-        return recursion(0,0,0,prices,dp);
+        // return recursion(0,0,0,prices,dp);
+
+        return tabulation(n,prices);
         
     }
 };
