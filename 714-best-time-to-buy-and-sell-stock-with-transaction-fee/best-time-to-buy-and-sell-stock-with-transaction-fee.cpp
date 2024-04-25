09608fec2506ea9359790bd274a264aefce7f2f5
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-25 23:31:49 +0530

    Time: 151 ms (42.86%) | Memory: 93.3 MB (40.66%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -25,13 +25,41 @@ public:
         return dp[index][buy] = profit;
     }
 
+    int tabulation(int n, int fee, vector<int> &prices){
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+
+        for(int index=n-1; index>=0; index--){
+            for(int buy=0; buy<2; buy++){
+
+                int profit = 0;
+
+                if(buy == 0){
+                    profit = max(dp[index+1][1] - prices[index],
+                    dp[index+1][0]);
+                }else{
+
+                    profit = max(dp[index+1][0] + prices[index] - fee,
+                    dp[index+1][1]);
+
+                }
+
+                 dp[index][buy] = profit;
+            }
+        }
+
+        return dp[0][0];
+
+    }
+
     int maxProfit(vector<int>& prices, int fee) {
 
         int n = prices.size();
 
-        vector<vector<int>> dp(n,vector<int>(2,-1));
+        // vector<vector<int>> dp(n,vector<int>(2,-1));
 
-        return recursion(0,0,fee,prices,dp);
+        // return recursion(0,0,fee,prices,dp);
+        return tabulation(n,fee,prices);
+
         
     }
 };
