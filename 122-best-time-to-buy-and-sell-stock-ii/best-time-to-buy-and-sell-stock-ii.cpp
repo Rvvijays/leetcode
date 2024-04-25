6fd6204d5cd2f4e50737e8bb832050a35367f4d5
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-25 21:27:20 +0530

    Time: 10 ms (20.02%) | Memory: 17.8 MB (18.20%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -13,8 +13,6 @@ public:
         }
 
 
-
-
         if(canBuy == 0){
             // i can buy or not buy.
             int buyy = -prices[index] + recursion(index+1,1,prices,dp);
@@ -28,11 +26,35 @@ public:
 
         return dp[index][canBuy]= max(sell, notSell);
     }
+
+    int tabulation(int n, vector<int> &prices) {
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+
+        for(int index = n-1; index>=0; index--){
+            for(int canBuy = 0; canBuy<2; canBuy++){
+                 if(canBuy == 0){
+                    int buyy = -prices[index] + dp[index+1][1];
+                    int notByt =dp[index+1][0];
+                    dp[index][canBuy] =  max(buyy,notByt);
+                }else{
+                    int sell = dp[index+1][0] + prices[index];
+                    int notSell = dp[index+1][1];
+
+                    dp[index][canBuy]= max(sell, notSell);
+                }
+            }
+        }
+        return dp[0][0];
+    }
+
+
     int maxProfit(vector<int>& prices) {
 
         int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
-        return recursion(0,0,prices,dp);
+        // vector<vector<int>> dp(n+1,vector<int>(2,-1));
+        // return recursion(0,0,prices,dp);
+
+        return tabulation(n,prices);
         
     }
 };
