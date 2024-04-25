8e6fdf82d62fdc90053bd8b779c8d93aebf2379c
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-26 00:07:36 +0530

    Time: 367 ms (21.24%) | Memory: 294.5 MB (24.76%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -22,14 +22,38 @@ public:
         return dp[index][curr]= recursion(index+1,curr,nums,dp);
 
     }
+
+    int tabulation(int n, vector<int> &nums){
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+
+        for(int index = n-1; index>=0; index--){
+            for(int curr=n; curr>=0; curr--){
+
+                if(curr == nums.size() || nums[index]>nums[curr]){
+                    dp[index][curr]= max(1 + dp[index+1][index],dp[index+1][curr]);
+                }else{
+                 dp[index][curr]= dp[index+1][curr];
+
+                }
+
+
+            }
+        }
+
+        return dp[0][n];
+        
+    }
     int lengthOfLIS(vector<int>& nums) {
 
         int n = nums.size();
 
 
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
 
-        return recursion(0,n,nums,dp);
+        // return recursion(0,n,nums,dp);
+
+        return tabulation(n,nums);
+
         
     }
 };
