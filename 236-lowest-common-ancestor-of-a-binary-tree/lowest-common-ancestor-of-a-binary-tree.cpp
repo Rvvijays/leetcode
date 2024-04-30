c3f8d150ac5307a97089259e663705ec39302152
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-30 16:10:59 +0530

    Time: 17 ms (25.53%) | Memory: 16.1 MB (49.16%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,121 +10,33 @@
 class Solution {
 public:
 
-    
-  
-    bool generate(TreeNode* root,TreeNode* target,vector<TreeNode*> &ans){
-        if(root == nullptr){
-            return false;
-        }
-
-        if(root == target){
-           
-            return true;
-        }
-
-        if(root->left!=nullptr){
-            ans.push_back(root->left);
-            if(generate(root->left,target,ans)){
-                return true;
-            }
-            ans.pop_back();
-        }
-
-        if(root->right!=nullptr){
-            ans.push_back(root->right);
-            if(generate(root->right,target,ans)){
-                return true;
-            }
-            ans.pop_back();
-        }
-
-        return false;
-    }
-
-    TreeNode* brute(TreeNode* root, TreeNode* p, TreeNode* q){
-        vector<TreeNode*> pathp;
-        vector<TreeNode*> pathq;
-        pathp.push_back(root);
-        pathq.push_back(root);
-        generate(root,p,pathp);
-        generate(root,q,pathq);
-
-        int i=0;
-        int j=0;
-
-        TreeNode* lca = nullptr;
-
-        while(i<pathp.size() && j<pathq.size()){
-
-            if(pathp[i] == pathq[j]){
-                lca = pathp[i];
-            }else{
-                break;
-            }
-
-            i++;
-            j++;
-
-        }
-
-       return lca;
-    }
-
-    TreeNode* traverse(TreeNode* root, TreeNode* p, TreeNode* q){
-        if(root == nullptr){
+    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q){
+        if(root== nullptr || root == p || root == q){
             return root;
         }
 
-        if(root == p){
-            return p;
-        }
+        TreeNode* left = lca(root->left,p,q);
+        TreeNode* right = lca(root->right,p,q);
 
-        if(root == q){
-            return q;
-        }
-
-        cout<<"root:"<<root->val<<endl;
-
-        if(root->left!=nullptr){
-            cout<<"left:"<<root->left->val<<endl;
-        }
-
-         if(root->right!=nullptr){
-            cout<<"right:"<<root->right->val<<endl;
-        }
-
-        TreeNode* left = traverse(root->left,p,q);
-
-        TreeNode* right = traverse(root->right,p,q);
-
-        // if(left!=nullptr && right!=nullptr && left == p && right == q){
-        //     cout<<"left:"<<left->val<<" right:"<<right->val<<endl;
-        //     return root;
-        // }
+       
 
         if((left == p && right == q) || (left == q && right == p)){
             return root;
         }
 
 
-
-    
-
-        if(left != nullptr){
-            return left;
+        if(left ==nullptr){
+            return right;
         }
 
-        return right;
+        if(right == nullptr){
+            return left;
+        }
+        return nullptr;
 
     }
-
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        return lca(root,p,q);
 
-    // return brute(root,p,q);
-
-    return traverse(root,p,q);
-
-
-    
     }
 };
