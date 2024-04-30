f062003ccbf25cf0ba8ce4242aabe235864a60d7
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-01 01:38:59 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (64.12%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -2,71 +2,70 @@ class Solution {
 public:
 
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();        
-        queue<pair<int,int>> q;
-
-        int totalOranges = 0;
-        int totalRotten = 0;
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]==2){
-                    q.push({i,j});
-                }
-                if(grid[i][j]!=0){
-                    totalOranges++;
-                }
-            }
-        }
-
-        vector<int> dx = {0,0,1,-1};
-        vector<int> dy = {1,-1,0,0};
-
-        int totalTime = 0;
-        // cout<<"totalRotten:"<<totalRotten<<endl;
-        // cout<<"totalOranges:"<<totalOranges<<endl;
-
-
-
-
-        while(!q.empty()){
-
-            int k = q.size();
-            totalRotten += k;
-
-            while(k--){
-                auto node = q.front();
-                q.pop();
-
-                for(int i=0; i<4; i++){
-                    int x = node.first + dx[i];
-                    int y = node.second + dy[i];
-
-                    if(x < 0 || y <0 || x >= n || y >= m || grid[x][y]!=1){
-                        continue;
-                    }
-
-                    grid[x][y] = 2;
-                    q.push({x,y});
-                    
-                }
-
-            }
-
-            if(!q.empty()){
-                totalTime++;
-            }
         
+	int n = grid.size();
+	int m = grid[0].size();
+	queue<pair<int,pair<int,int>>> q;
+
+    int totalOranges = 0;
+    int orangeRot = 0;
+
+	for(int i=0; i<n; i++){
+		for(int j=0; j<m; j++){
+			if(grid[i][j] == 2){
+				q.push({i,{j,0}});
+                totalOranges++;
+                orangeRot++;
+			}
+
+            if(grid[i][j] == 1){
+                totalOranges++;
+            }
 
 
-        }
+		}
+	}
 
-        if(totalOranges == totalRotten){
-            return totalTime;
-        }
+    if(totalOranges==0){
+        return 0;
+    }
 
+    if(orangeRot == 0){
         return -1;
+    }
+	
+	int dx[4] = {0,0,1,-1};
+	int dy[4] = {1,-1,0,0};
+	
+	int ans = 0;
+
+	
+	while(!q.empty()){
+		auto it = q.front();
+        q.pop();
+
+        int i = it.first;
+        int j = it.second.first;
+        int minute = it.second.second;
+		
+		ans = minute;
+		
+		for(int k=0; k<4; k++){
+			int x = i + dx[k];
+			int y = j + dy[k];
+			
+			if(x<0 || y <0 || x >= n || y>=m || grid[x][y]!=1){
+				continue;
+			}
+            grid[x][y] = 2;
+            orangeRot++;
+			
+			q.push({x,{y,minute+1}});
+		}
+		
+	}
+	
+	return totalOranges == orangeRot ? ans:-1;
 
         
     }
