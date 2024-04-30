c99698c1857d1ed83a7bfa524444d86a81b06d6d
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-04-30 11:57:09 +0530

    Time: 8 ms (41.88%) | Memory: 17.2 MB (97.67%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -11,65 +11,14 @@
  */
 class Solution {
 public:
-
-
-    int iterative(TreeNode* root){
- if(root==nullptr){
-            return 0;
-        }
-        queue<TreeNode*> q;
-        int depth = 0;
-
-
-        q.push(root);
-
-        while(!q.empty()){
-           
-            int size = q.size();
-            // cout<<"level: "<<depth<<": ";
-            for(int i=0; i<size; i++){
-                TreeNode* temp = q.front();
-                // cout<<temp->val;
-                q.pop();
-
-                if(temp->left!=nullptr){
-                    q.push(temp->left);
-                }
-
-                if(temp->right!=nullptr){
-                    q.push(temp->right);
-                }
-
-            }
-
-            depth++;
-
-        }
-
-
-        return depth;
-
-    }
-
-    int recursive(TreeNode* root){
-        if(root==nullptr){
-            return 0;
-        }
-
-        int left = recursive(root->left);
-        int right = recursive(root->right);
-
-        return 1 + max(left,right);
-    }
-
-    
     int maxDepth(TreeNode* root) {
-
-        // return iterative(root);
-
-        return recursive(root);
-
-       
-        
+        	if(root==nullptr){
+		return 0;
+	}
+	
+	int left = maxDepth(root->left);
+	int right = maxDepth(root->right);
+	
+	return 1 + max(left,right);
     }
 };
