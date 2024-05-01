3abfd3e39e918d1088ceae215def7472bc8b4951
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-01 17:44:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (27.20%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -18,10 +18,35 @@ public:
                        recursion(n - 1, dp);
     }
 
+    int tabulation(int n){
+if (n == 0 || n < 0) {
+            return 0;
+        }
+
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        vector<int> dp(n+1,0);
+
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        }
+
+        return dp[n];
+
+    }
+
     int tribonacci(int n) {
 
-        vector<int> dp(n+1, -1);
+        // vector<int> dp(n+1, -1);
 
-        return recursion(n, dp);
+        // return recursion(n, dp);
+
+        return tabulation(n);
+        
     }
 };
