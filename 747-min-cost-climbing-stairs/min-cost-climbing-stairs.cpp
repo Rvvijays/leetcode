855c62b6704d4e5b69c91598348688f162423813
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-01 18:11:28 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (60.70%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
 
     int recursion(int index, vector<int> &cost, vector<int> &dp){
-
-        // pay
-
       
 
         if(index >= cost.size()){
@@ -19,10 +16,27 @@ public:
 
     
     }
+
+    int tabulation(vector<int>& cost){
+        int n = cost.size();
+        vector<int> dp(n+2,0);
+
+        for(int index=n-1; index>=0; index--){
+
+            dp[index] = cost[index] +   min(dp[index+1],dp[index+2]);
+        }
+
+        return min(dp[0], dp[1]);
+
+
+
+    }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         // vector<
-        vector<int> dp(n,-1);
-        return min(recursion(0, cost,dp), recursion(1,cost,dp));
+        // vector<int> dp(n,-1);
+        // return min(recursion(0, cost,dp), recursion(1,cost,dp));
+
+        return tabulation(cost);
     }
 };
