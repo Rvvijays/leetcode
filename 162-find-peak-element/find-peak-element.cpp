0205fe34143be94302369199f0e1e80897fca2f2
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-01 16:41:40 +0530

    Time: 3 ms (64.48%) | Memory: 11.3 MB (50.71%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,72 +1,37 @@
 class Solution {
 public:
-
-    int brute(vector<int> &nums) {
+    int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1){
+
+        if (n == 1) {
             return 0;
         }
 
-        if(nums[0] > nums[1]){
+        if (nums[0] > nums[1]) {
             return 0;
         }
 
-        if(nums[n-1]> nums[n-2]){
-            return n-1;
-        }
-
-        for(int i=1; i<n-1; i++){
-            int a = nums[i];
-
-            if(a > nums[i-1] && a > nums[i+1]){
-                return i;
-            }
-        }
-
-        return -1;
-    }
-
-    int optimal(vector<int> &nums) {
-         int n = nums.size();
-        if(n == 1){
-            return 0;
-        }
-
-        if(nums[0] > nums[1]){
-            return 0;
-        }
-
-        if(nums[n-1]> nums[n-2]){
+        if (nums[n - 1] > nums[n - 2]) {
             return n-1;
         }
 
         int low = 1;
-        int high = n-2;
+        int high = n - 2;
 
-        while(low<=high){
-            int mid = (low+high)/2;
+        while (low <= high) {
+            int mid = (high-low) / 2 + low;
 
-            if(nums[mid]> nums[mid-1] && nums[mid]>nums[mid+1]){
+            if (nums[mid -1] <= nums[mid] && nums[mid] >= nums[mid+1]) {
                 return mid;
             }
 
-            if(nums[mid-1] < nums[mid]  && nums[mid] < nums[mid+1]){
+            if (nums[mid-1] <= nums[mid] && nums[mid] <= nums[mid+1]) {
                 low = mid + 1;
-            }else{
+            } else {
                 high = mid - 1;
             }
         }
 
         return -1;
-
-
-    }
-
-    int findPeakElement(vector<int>& nums) {
-
-        // return brute(nums);
-
-        return optimal(nums);
-        
     }
 };
