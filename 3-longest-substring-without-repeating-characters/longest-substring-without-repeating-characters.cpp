7a0b3e785c2f082635893f8c7a41cf3d22ff179e
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-06 23:44:25 +0530

    Time: 8 ms (79.32%) | Memory: 11.3 MB (50.94%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,34 +1,65 @@
 class Solution {
 public:
-    
-    int lengthOfLongestSubstring(string s) {
+    int brute(string s) {
         int n = s.size();
-        if(n == 0){
+        if (n == 0) {
             return 0;
         }
-        unordered_map<char,bool> hash;
+        unordered_map<char, bool> hash;
         int maxi = 1;
-        
-        for(int i=0; i<n; i++){
+
+        for (int i = 0; i < n; i++) {
             hash.clear();
             hash[s[i]] = true;
             int count = 1;
-           for(int j=i+1; j<n; j++){
-               if(hash[s[j]]){
-                   break;
-               }else{
-                   count++;
-                   hash[s[j]] = true;
-               }
-           }
-                maxi = max(maxi,count);
-            
-            
+            for (int j = i + 1; j < n; j++) {
+                if (hash[s[j]]) {
+                    break;
+                } else {
+                    count++;
+                    hash[s[j]] = true;
+                }
+            }
+            maxi = max(maxi, count);
         }
-        
-        // maxi = max(maxi,count);
-        
+
         return maxi;
-        
+    }
+
+    int lengthOfLongestSubstring(string s) {
+
+        int n = s.length();
+
+        int left = 0;
+        int right = 0;
+
+        unordered_map<char,int> hash;
+        int maxLength = 0;
+
+        while(right < n){
+
+            if(hash.find(s[right]) != hash.end()){
+                int prevIndex = hash[s[right]];
+
+                if(prevIndex >= left){
+                    left = prevIndex + 1;
+                }
+
+            }
+
+            hash[s[right]] = right;
+
+
+            int length = right - left + 1;
+
+            maxLength = max(maxLength,length);
+
+            right++;
+
+
+
+        }
+
+        return maxLength;
     }
 };
