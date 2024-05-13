cdd8f274df964b03ae1c2bbd1b653caa037fa93b
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-13 15:52:05 +0530

    Time: 16 ms (51.83%) | Memory: 20.1 MB (60.90%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        
+
+    int brute(vector<int>& ratings) {
         int n = ratings.size();
-        
+
         vector<int> leftToRight(n,1);
         vector<int> rightToleft(n,1);
         for (int i = 1; i < n; i++) {
@@ -25,6 +25,32 @@ public:
         }
         
         return sum;
+    }
+    int candy(vector<int>& ratings) {
+        
+        int n = ratings.size();
+
+        vector<int> candies(n,1);
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if(ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i],candies[i+1] + 1);
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += candies[i];
+        }
+
+        return sum;
+        
+       
         
     }
 };
