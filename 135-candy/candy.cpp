15373b6877df0961ee161b92cf4a5eda1cb8b1f4
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-13 15:52:36 +0530

    Time: 16 ms (51.83%) | Memory: 20 MB (60.90%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -36,17 +36,18 @@ public:
                 candies[i] = candies[i - 1] + 1;
             }
         }
+        int sum = candies[n-1];
 
         for (int i = n - 2; i >= 0; i--) {
             if(ratings[i] > ratings[i + 1]) {
                 candies[i] = max(candies[i],candies[i+1] + 1);
             }
+            sum += candies[i];
+
         }
 
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += candies[i];
-        }
+        // for (int i = 0; i < n; i++) {
+        // }
 
         return sum;
         
