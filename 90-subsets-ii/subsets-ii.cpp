d189ab3f13d25a39883892a3ba6d637a90dacea8
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-15 02:59:54 +0530

    Time: 3 ms (56.21%) | Memory: 9.2 MB (44.06%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,61 +1,40 @@
+
 class Solution {
 public:
 
-    void generate(int index, vector<int> &curr, vector<int> &arr, set<vector<int>> &ans){
+    void generate(int index, vector<int> &curr, vector<int>& nums, set<vector<int>> &ans){
+        if(index == nums.size()) {
+            // if(curr.size()>0) {
+                ans.insert(curr);
+            // }
 
-        if(index == arr.size()){
-            ans.insert(curr);
             return;
         }
 
-        curr.push_back(arr[index]);
-        generate(index+1,curr,arr,ans);
+
+        curr.push_back(nums[index]);
+        generate(index+1,curr,nums,ans);
         curr.pop_back();
-        generate(index+1,curr,arr,ans);
+        generate(index+1,curr,nums,ans);
     }
 
-    void optimal(int index, vector<int> &curr, vector<int> &arr, vector<vector<int>> &ans){
 
-        ans.push_back(curr);
-
-
-        for(int i=index; i<arr.size(); i++){
-            if(i>index && arr[i]==arr[i-1]){
-                continue;
-            }
-
-            curr.push_back(arr[i]);
-            optimal(i+1,curr,arr,ans);
-            curr.pop_back();
-        }
-        
-
-
-    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
 
-        // set<vector<int>> ans;
-        // vector<int> curr;
+        sort(nums.begin(), nums.end());
+       set<vector<int>> ans;
 
-        sort(nums.begin(),nums.end());
-
-        // generate(0,curr,nums,ans);
-
-        // vector<vector<int>> anss;
-        // for (auto i : ans) {
-        //     anss.push_back(i);
-        // }
-
-        // return anss;
-
-
-        vector<vector<int>> ans;
         vector<int> curr;
+       generate(0,curr, nums,ans);
 
-        optimal(0,curr,nums,ans);
-        return ans;
+       vector<vector<int>> ss;
 
+        
+       for(auto it : ans) {
+        ss.push_back(it);
+       }
 
+       return ss; 
         
     }
 };
