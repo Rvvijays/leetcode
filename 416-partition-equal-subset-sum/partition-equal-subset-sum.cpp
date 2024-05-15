baa59676860cfe6bb0982e2d6fe54d7570e8bfb0
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-15 23:29:46 +0530

    Time: 348 ms (11.64%) | Memory: 16.1 MB (65.20%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -4,11 +4,15 @@ public:
     bool recursion(int index, int target, vector<int>& nums,vector<vector<int>> &dp) {
 
         if(index == nums.size()-1) {
-            if(target == 0) {
+            if(target == 0 || nums[index] == target) {
                 return true;
             }
 
             return false;
+        }
+
+        if(target == 0) {
+            return true;
         }
 
         if(dp[index][target]!=-1) {
@@ -38,10 +42,30 @@ public:
             return false;
         }
 
-        int target = totalSum/2;
+        int sum = totalSum/2;
 
-        vector<vector<int>> dp(n,vector<int>(target+1,-1));
+        // vector<vector<int>> dp(n,vector<int>(target+1,-1));
 
-        return recursion(0,target,nums,dp);
+        // return recursion(0,target,nums,dp);
+
+
+        vector<vector<bool>> dp(n,vector<bool>(sum+1,false));
+        dp[n-1][0] = true;
+        for(int index = n-2; index>=0; index--) {
+            for(int target = 0; target <= sum; target++) {
+                int take = false;
+                if(target >= nums[index]) {
+                    take = dp[index+1][target - nums[index]];
+                }
+
+                bool nottake = dp[index+1][target];
+
+                dp[index][target] = take || nottake;
+            }
+        }
+
+        return dp[0][sum];
+
+
     }
 };
