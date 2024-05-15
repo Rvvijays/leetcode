5ee291889d6fe31118f50a8444f19ce763e88f68
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-15 14:50:05 +0530

    Time: 609 ms (5.03%) | Memory: 295 MB (8.95%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -22,16 +22,14 @@ public:
             return 0;
         }
         
-        if(dp[index][prev] !=-1) {
-            return dp[index][prev];
+        if(dp[index][prev+1] !=-1) {
+            return dp[index][prev+1];
         }
 
-        int take = 0;
-        if(prev == 0 || nums[prev-1] < nums[index-1]){
-            take = 1 + recursion(index+1,index,n,nums,dp);
+        if(prev == -1 || nums[prev] < nums[index]){
+            return dp[index][prev+1] = max(1 + recursion(index+1,index,n,nums,dp), recursion(index+1,prev,n,nums,dp));
         }
-        int notTake = recursion(index+1,prev,n,nums,dp);
-        return dp[index][prev] = max(take,notTake);
+        return dp[index][prev+1] = recursion(index+1,prev,n,nums,dp);
     }
 
 
@@ -39,9 +37,9 @@ public:
         
         int n = nums.size();
 
-    //     vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    //     vector<vector<int>> dp(n+1,vector<int>(n+1,1));
 
-    //    for(int index = n-1; index >= 0; index--) {
+    //    for(int index = n; index > 0; index--) {
     //     for(int prev = 0; prev <= index; prev++) {
     //         if(prev == 0 || nums[prev-1] < nums[index-1]){
     //             dp[index][prev] = max(1 + dp[index+1][index], dp[index+1][prev]);
@@ -52,17 +50,13 @@ public:
     //     }
     //    }
 
-    //    for(int i=0; i<n; i++) {
-    //     for(int j=0; j<n; j++) {
-    //         cout<<dp[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    //    }
-
+   
     //    return dp[1][0];
 
-vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return recursion(1,0,n+1,nums,dp);
+
+        // vector<int> curr;
+          vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return recursion(0,-1,n,nums,dp);
 
     }
 };
