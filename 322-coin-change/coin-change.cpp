bf27b66773a03594f48932c723bd630efaee7a8b
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-15 23:00:28 +0530

    Time: 55 ms (17.38%) | Memory: 47.1 MB (25.24%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,123 +1,41 @@
 class Solution {
 public:
-    int recursion(int index, int target, vector<int> &coins){
-        if(index == 0){
-            if(target % coins[index] == 0){
-                return target / coins[index];
+
+    int recursion(int index, int amount, vector<int> &coins, vector<vector<int>> &dp) {
+
+        if(index == coins.size()-1) {
+            if(amount == 0) {
+                return 0;
+            }
+
+            if(amount % coins[index]==0){
+                return amount/coins[index];
             }
 
             return 1e9;
         }
 
-        int notTake = recursion(index-1,target,coins);
-
+        if(dp[index][amount]!=-1) {
+            return dp[index][amount];
+        }
+        
         int take = 1e9;
-
-        if(target >= coins[index]){
-            take = 1 + recursion(index,target - coins[index], coins);
+        if(amount >= coins[index]) {
+            take = 1 + recursion(index,amount - coins[index], coins,dp);
         }
 
-        return min(take,notTake);
+        int nottake = recursion(index+1, amount, coins,dp);
+
+        return dp[index][amount] = min(take,nottake);
     }
-
-    int memorization(int index, int target, vector<int> &coins, vector<vector<int>> &dp){
-        if(index == 0){
-            if(target % coins[index] == 0){
-                return target / coins[index];
-            }
-
-            return 1e9;
-        }
-
-        if(dp[index][target]!=-1){
-            return dp[index][target];
-        }
-
-        int notTake = memorization(index-1,target,coins,dp);
-
-        int take = 1e9;
-
-        if(target >= coins[index]){
-            take = 1 + memorization(index,target - coins[index], coins,dp);
-        }
-
-        return dp[index][target] =  min(take,notTake);
-    }
-
-
-    int tabulation(int n, int amount, vector<int> &coins){
-
-        vector<vector<int>> dp(n,vector<int>(amount+1,1e9));
-
-        for(int i=0; i<=amount; i++){
-            if(i % coins[0]==0){
-                dp[0][i] = i / coins[0];
-            }
-        }
-
-        for(int index=1; index<n; index++){
-            for(int target=0; target<=amount; target++){
-                int notTake = dp[index-1][target];
-
-                int take = 1e9;
-
-                if(target >= coins[index]){
-                    take = 1 + dp[index][target - coins[index]];
-                }
-
-                dp[index][target] =  min(take,notTake);
-            }
-        }
-
-        return dp[n-1][amount];
-
-      
-    }
-
-     int spaceoptimization(int n, int amount, vector<int> &coins){
-
-        // vector<vector<int>> dp(n,vector<int>(amount+1,1e9));
-        vector<int> prev(amount+1,1e9);
-
-        for(int i=0; i<=amount; i++){
-            if(i % coins[0]==0){
-                prev[i] = i / coins[0];
-            }
-        }
-
-        for(int index=1; index<n; index++){
-            vector<int> curr(amount+1,1e9);
-            for(int target=0; target<=amount; target++){
-                int notTake = prev[target];
-
-                int take = 1e9;
-
-                if(target >= coins[index]){
-                    take = 1 + curr[target - coins[index]];
-                }
-
-                curr[target] =  min(take,notTake);
-            }
-            prev =curr;
-        }
-
-        return prev[amount];
-
-      
-    }
-
     int coinChange(vector<int>& coins, int amount) {
+
         int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
 
-        // int count =  recursion(n-1,amount,coins);
+        int ans = recursion(0, amount, coins,dp);
 
-        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        // int count =  memorization(n-1,amount,coins,dp);
-
-        int count =  spaceoptimization(n,amount,coins);
-
-        return count >= 1e9 ? -1 : count;
-
+        return ans ==1e9 ? -1 : ans;
         
     }
 };
