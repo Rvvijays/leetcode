e8754db57b455af68884ff0c385d9ebcfd9c5002
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-15 22:04:42 +0530

    Time: 8 ms (15.81%) | Memory: 10 MB (21.12%) - LeetSync

--- 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
     int recursion(int i, int j, vector<int>& values, vector<vector<int>> &dp) {
-        if(i>=j){
+        if(i+1 == j){
             return 0;
         }
 
@@ -10,22 +10,24 @@ public:
             return dp[i][j];
         }
 
-        int mini = INT_MAX;
+        int mini = 1e9;
 
-        for(int k=i; k<j; k++) {
-            int temp = values[i-1] * values[k] * values[j] + recursion(i,k,values,dp) + recursion(k+1,j,values,dp);
+        for(int k=i+1; k<j; k++) {
+            int temp = values[i] * values[k] * values[j] 
+            + recursion(i,k,values,dp) 
+            + recursion(k,j,values,dp);
 
             mini = min(temp,mini);
         }
 
-        return dp[i][j] = mini ;
+        return dp[i][j] =  mini ;
     }
     int minScoreTriangulation(vector<int>& values) {
         int n = values.size();
 
-       vector<vector<int>> dp(n, vector<int>(n,-1));
+       vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
 
-        return recursion(1,values.size()-1,values,dp);
+        return recursion(0,n-1,values,dp);
         
     }
 };
