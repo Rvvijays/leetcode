873d3cd5784a045b75e17b7925fe15aced5175c4
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-15 23:05:22 +0530

    Time: 60 ms (13.70%) | Memory: 46.7 MB (29.93%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -28,14 +28,41 @@ public:
 
         return dp[index][amount] = min(take,nottake);
     }
-    int coinChange(vector<int>& coins, int amount) {
+    int coinChange(vector<int>& coins, int target) {
 
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1,-1));
+        // vector<vector<int>> dp(n, vector<int>(amount+1,-1));
 
-        int ans = recursion(0, amount, coins,dp);
+        // int ans = recursion(0, amount, coins,dp);
 
-        return ans ==1e9 ? -1 : ans;
+        // return ans ==1e9 ? -1 : ans;
+        vector<vector<int>> dp(n, vector<int>(target+1,1e9));
+
+        for(int amount = 0; amount <= target; amount++) {
+            if(amount == 0) {
+                 dp[n-1][amount] = 0;
+            }
+
+            if(amount % coins[n-1]==0){
+                dp[n-1][amount] =amount/coins[n-1];
+            }
+        }
+
+
+        for(int index = n-2; index>=0; index--) {
+            for(int amount = 0; amount<= target; amount++) {
+                int take = 1e9;
+                if(amount >= coins[index]) {
+                    take = 1 + dp[index][amount - coins[index]];
+                }
+
+                int nottake = dp[index+1][amount];
+
+                dp[index][amount] = min(take,nottake);
+            }
+        }
+
+        return dp[0][target]==1e9?-1:dp[0][target];
         
     }
 };
