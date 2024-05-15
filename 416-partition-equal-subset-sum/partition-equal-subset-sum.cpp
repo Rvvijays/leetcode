5cda53245b0480780a92dbd016c86055002006f6
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-15 23:23:54 +0530

    Time: 169 ms (47.82%) | Memory: 98.8 MB (26.13%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,178 +1,47 @@
 class Solution {
 public:
-    bool recursion(int index, int sumA, int sumB,vector<int>& nums){
-        if(index == nums.size()){
-            if(sumA == sumB){
-                return true;
-            }
 
-            return false;
-        }
+    bool recursion(int index, int target, vector<int>& nums,vector<vector<int>> &dp) {
 
-
-        bool case1 = recursion(index + 1, sumA + nums[index], sumB, nums);
-        bool case2 = recursion(index + 1, sumA, sumB + nums[index], nums);
-
-        return case1 || case2;
-    }
-
-    bool memorization(int index, int sumA, int sumB,vector<int>& nums ,vector<vector<int>> &dp1, vector<vector<int>> &dp2){
-        if(index == nums.size()){
-            if(sumA == sumB){
-                return true;
-            }
-
-            return false;
-        }
-
-        if(dp1[index][sumA]!=-1 && dp2[index][sumB]!=-1){
-            return dp1[index][sumA] || dp2[index][sumB];
-        }
-
-
-        bool case1 = memorization(index + 1, sumA + nums[index], sumB, nums,dp1,dp2);
-        bool case2 = memorization(index + 1, sumA, sumB + nums[index], nums,dp1,dp2);
-        dp1[index][sumA] = case1;
-        dp2[index][sumB] = case2;
-
-        return case1 || case2;
-    }
-
-    bool tabulation(vector<int>& nums){
-        int n = nums.size();
-        vector<vector<bool>> dp1(n+1,vector<bool>(20000,false));
-        vector<vector<bool>> dp2(n+1,vector<bool>(20000,false));
-
-        for(int index = n; index>=0; index--){
-            for(int sumA=0; sumA<20001; sumA++){
-                for(int sumB=0; sumB<20001; sumB++){
-
-                    if(index == n){
-                        if(sumA == sumB){
-                            dp1[index][sumA] = true;
-                            dp2[index][sumB] = true;
-                        }
-                        continue;
-                    }
-
-                    bool case1 = dp1[index + 1][sumA + nums[index]];
-                    bool case2 = dp2[index + 1][sumB + nums[index]];
-
-                    dp1[index][sumA] = case1;
-                    dp2[index][sumB] = case2;
-
-                    // return case1 || case2;
-                }
-            }
-        }
-
-        return dp1[0][0] && dp2[0][0];
-
-
-    }
-
-    bool recursion2(int index, int target, vector<int> &nums, vector<vector<int>> &dp){
         if(index == nums.size()-1) {
-            if(target == 0){
+            if(target == 0) {
                 return true;
             }
 
             return false;
         }
 
-        if(dp[index][target]!=-1){
+        if(dp[index][target]!=-1) {
             return dp[index][target];
         }
 
         int take = false;
-        if(target >= nums[index]){
-            take = recursion2(index + 1, target - nums[index], nums,dp);
+        if(target >= nums[index]) {
+            take = recursion(index+1,target - nums[index], nums, dp);
         }
-        int notTake = recursion2(index + 1, target, nums,dp);
 
-        return dp[index][target] = take || notTake;
+        bool nottake = recursion(index+1, target, nums, dp);
+
+        return dp[index][target] = take || nottake;
 
     }
-
-
-
     bool canPartition(vector<int>& nums) {
-        // return recursion(0,0,0,nums);
+        int totalSum = 0;
 
         int n = nums.size();
-        // vector<vector<int>> dp1(n,vector<int>(20000,-1));
-        // vector<vector<int>> dp2(n,vector<int>(20000,-1));
-        // return memorization(0,0,0,nums,dp1,dp2);
 
-        // return tabulation(nums);
-
-        int totSum = 0;
-        for(int i=0; i<n; i++){
-            totSum += nums[i];
+        for(int i=0; i<n; i++) {
+            totalSum += nums[i];
         }
 
-        if(totSum%2==1){
+        if(totalSum%2 !=0){
             return false;
         }
 
-        int target1 = totSum/2;
+        int target = totalSum/2;
 
-        // vector<vector<int>> dp(n,vector<int>(target+1,-1));
-        // return recursion2(0,target,nums,dp);
+        vector<vector<int>> dp(n,vector<int>(target+1,-1));
 
-        // vector<vector<bool>> dp(n,vector<bool>(target1+1,false));
-
-        // for(int index=n-1; index>=0; index--){
-        //     for(int target =0; target<=target1; target++){
-        //         if(index == n -1){
-        //             if(target == 0){
-        //                 dp[index][target] = true;
-        //             }
-        //             continue;
-        //         }
-
-        //         int take = false;
-        //         if(target >= nums[index]){
-        //             take = dp[index + 1][target - nums[index]];
-        //         }
-        //         int notTake = dp[index + 1][target];
-
-        //         dp[index][target] = take || notTake;
-        //     }
-        // }
-
-        // return dp[0][target1];
-
-
-        vector<bool> prev(target1+1,false);
-
-        for(int index=n-1; index>=0; index--){
-            vector<bool> curr(target1+1,false);
-            for(int target =0; target<=target1; target++){
-                if(index == n - 1){
-                    if(target == 0){
-                        curr[target] = true;
-                    }
-                    continue;
-                }
-
-                int take = false;
-                if(target >= nums[index]){
-                    take = prev[target - nums[index]];
-                }
-                int notTake = prev[target];
-
-                curr[target] = take || notTake;
-            }
-
-            prev = curr;
-        }
-
-        return prev[target1];
-
-
-        
-
-        
+        return recursion(0,target,nums,dp);
     }
 };
