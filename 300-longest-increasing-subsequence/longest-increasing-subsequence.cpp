698d267c6f1fc4c3c92d69cd3386237db794d8e6
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-15 14:04:49 +0530

    Time: 619 ms (5.03%) | Memory: 294.9 MB (8.95%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,77 +1,45 @@
 class Solution {
 public:
+    // void recursion(int index, vector<int> curr, vector<int> &nums) {
+    //     if(index == nums.size()) {
+    //         for(int i=0; i<curr.size(); i++) {
+    //             cout<<curr[i]<<" ";
+    //         }
+    //         cout<<endl;
+    //         return;
+    //     }
 
-    int recursion(int index,int curr, vector<int> &nums,vector<vector<int>> &dp) {
+    //     curr.push_back(nums[index]);
+    //     recursion(index+1,curr,nums);
 
-        if(index==nums.size()){
+    //     curr.pop_back();
+    //     recursion(index+1,curr,nums);
+        
+    // }
+
+    int recursion(int index, int prev, int n, vector<int> &nums,vector<vector<int>> &dp) {
+        if(index == n) {
             return 0;
         }
-
-
-        if(dp[index][curr]!=-1){
-            return dp[index][curr];
-        }
-
-
-
-        // take
-        if(curr == nums.size() || nums[index]>nums[curr]){
-            return dp[index][curr]= max(1 + recursion(index+1,index,nums,dp),recursion(index+1,curr,nums,dp));
-        }
-
-        return dp[index][curr]= recursion(index+1,curr,nums,dp);
-
-    }
-
-    int tabulation(int n, vector<int> &nums){
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-
-        for(int index = n-1; index>=0; index--){
-            for(int curr=n; curr>=0; curr--){
-
-                if(curr == nums.size() || nums[index]>nums[curr]){
-                    dp[index][curr]= max(1 + dp[index+1][index],dp[index+1][curr]);
-                } else {
-                 dp[index][curr]= dp[index+1][curr];
-
-                }
-
-
-            }
-        }
-
-        return dp[0][n];
         
-    }
-    int lengthOfLIS(vector<int>& nums) {
+        if(dp[index][prev] !=-1) {
+            return dp[index][prev];
+        }
 
+        if(prev == 0 || nums[prev-1] < nums[index-1]){
+            return dp[index][prev] = max(1 + recursion(index+1,index,n,nums,dp), recursion(index+1,prev,n,nums,dp));
+        }
+        return dp[index][prev] = recursion(index+1,prev,n,nums,dp);
+    }
+
+
+    int lengthOfLIS(vector<int>& nums) {
+        
         int n = nums.size();
 
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // vector<int> curr;
+        return recursion(1,0,n+1,nums,dp);
 
-        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-
-        // return recursion(0,n,nums,dp);
-
-        // return tabulation(n,nums);
-
-        // optimal approach
-
-        vector<int> dp(n,1);
-
-        int maxi = 0;
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(nums[j]<nums[i]){
-                    dp[i] = max(1 + dp[j], dp[i]);
-                }
-            }
-
-            maxi = max(maxi, dp[i]);
-        }
-
-        return maxi;
-
-        
     }
 };
