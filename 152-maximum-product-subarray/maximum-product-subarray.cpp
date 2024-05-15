648612ff825974c2bd0b02bd34605273f8e14b28
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-15 06:13:48 +0530

    Time: 43 ms (8.67%) | Memory: 16.2 MB (81.87%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,23 +1,50 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
-
+    int brute(vector<int>& nums) {
         int n = nums.size();
 
         int maxProd = nums[0];
-        for(int i =0; i<n; i++) {
-                int prod = 1;
-            for(int j = i; j<n; j++) {
+        for (int i = 0; i < n; i++) {
+            int prod = 1;
+            for (int j = i; j < n; j++) {
                 prod *= nums[j];
 
-                maxProd = max(maxProd,prod);
+                maxProd = max(maxProd, prod);
 
                 // cout<<"prod: "<<prod<<endl;
             }
         }
 
         return maxProd;
-        
-        
+    }
+
+    int maxProduct(vector<int>& nums) {
+
+        int n = nums.size();
+
+        int suffix = 1;
+        int prefix = 1;
+
+        int maxProd = nums[0];
+        for(int i=0; i<n; i++) {
+
+            if(prefix == 0) {
+                prefix =1;
+            }
+
+            if(suffix == 0){
+                suffix = 1;
+            }
+            prefix *= nums[i];
+            suffix *= nums[n-i-1];
+
+            cout<<"prefix:"<<prefix<<endl;
+            cout<<"suffix:"<<suffix<<endl;
+
+            maxProd = max(maxProd ,max(prefix, suffix));
+
+        }
+
+        return maxProd;
     }
 };
