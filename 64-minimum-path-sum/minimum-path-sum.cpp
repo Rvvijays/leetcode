94cce328b5d51e24142c4e7c6f20bcd69c65c039
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-15 22:24:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (41.35%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,116 +1,33 @@
 class Solution {
 public:
+    int recursion(int i, int j, int n, int m, vector<vector<int>>& grid, vector<vector<int>> &dp)  {
 
-    int recursion(int i, int j, vector<vector<int>>& grid){
-        if(i ==0 && j == 0){
+        if(i == n-1 && j == m-1) {
             return grid[i][j];
         }
 
-        if(i<0 || j<0){
+        if(i >= n || j >= m) {
             return INT_MAX;
         }
 
-        int left  = recursion(i-1,j,grid);
-        int right = recursion(i,j-1,grid);
-
-        return min(left,right)+grid[i][j];
-       
-    }
-    int memorization(int i, int j, vector<vector<int>>& grid,vector<vector<int>>& dp){
-        if(i ==0 && j == 0){
-            return grid[i][j];
-        }
-
-        if(i<0 || j<0){
-            return INT_MAX;
-        }
-
-        if(dp[i][j]!=-1){
+        if(dp[i][j] != -1) {
             return dp[i][j];
         }
 
-        int left  = memorization(i-1,j,grid,dp);
-        int right = memorization(i,j-1,grid,dp);
+        int right =  recursion(i,j+1,n,m,grid,dp);
+        int bottom = recursion(i+1,j,n,m,grid,dp);
 
-        return dp[i][j] = min(left,right)+grid[i][j];
-       
-    }
-    int tabulation(int n, int m, vector<vector<int>>& grid){
-        vector<vector<int>> dp(n,vector<int>(m,0));
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-
-                if(i==0 && j ==0){
-                    dp[i][j] = grid[i][j];
-                    continue;
-                }
-                int left = 300;
-                int right = 300;
-
-                if(i>0){
-                    left = dp[i-1][j];
-                }
-
-                if(j>0){
-                    right = dp[i][j-1];
-                }
-
-                dp[i][j] = min(left,right) + grid[i][j];
-            }
-        }
-
-        return dp[n-1][m-1];
-
-
-    }
-
-    int spaceoptimization(int n, int m, vector<vector<int>>& grid){
-        // vector<vector<int>> dp(n,vector<int>(m,0));
-        vector<int> prev(m,0);
-        for(int i=0; i<n; i++){
-                vector<int> curr(m,0);
-
-            for(int j=0; j<m; j++){
-
-                if(i==0 && j ==0){
-                    curr[j] = grid[i][j];
-                    continue;
-                }
-                int left = 300;
-                int right = 300;
-
-                if(i>0){
-                    left = prev[j];
-                }
-
-                if(j>0){
-                    right = curr[j-1];
-                }
-
-                curr[j] = min(left,right) + grid[i][j];
-            }
-                prev = curr;
-
-        }
-
-        return prev[m-1];
-
+        return dp[i][j] = min(right,bottom) + grid[i][j];
 
     }
     int minPathSum(vector<vector<int>>& grid) {
+
         int n = grid.size();
         int m = grid[0].size();
 
-        
+        vector<vector<int>> dp(n,vector<int>(m,-1));
 
-        // return recursion(n-1,m-1,grid);
-
-        // vector<vector<int>> dp(n,vector<int>(m,-1));
-        // return memorization(n-1,m-1,grid,dp);
-
-        return tabulation(n,m,grid);
-
-
+        return recursion(0,0,n,m,grid,dp);
         
     }
 };
