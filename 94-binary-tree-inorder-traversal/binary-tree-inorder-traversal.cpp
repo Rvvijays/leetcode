c273ecac206ca505fd9ca442ab998716a1be4755
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-17 05:20:51 +0530

    Time: 4 ms (31.84%) | Memory: 9.9 MB (40.07%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -12,38 +12,7 @@
 class Solution {
 public:
 
-    vector<int> iterative(TreeNode* root){
-        stack<TreeNode*> st;
-        vector<int> ans;
-         if(root==nullptr){
-            return ans;
-        }
-
-        TreeNode* curr = root;       
-
-        while(true){
-           
-           if(curr!=nullptr){
-                st.push(curr);
-                curr = curr->left;
-           }else{
-                if(st.empty()){
-                    break;
-                }
-                curr = st.top();
-                st.pop();
-
-                ans.push_back(curr->val);
-                curr = curr->right;
-                
-           }
-
-        }
-        return ans;
-
-    }
-
-    void recursive(TreeNode* root,vector<int>& ans){
+    void recursive(TreeNode* root,vector<int> &ans) {
         if(root==nullptr){
             return;
         }
@@ -51,52 +20,15 @@ public:
         recursive(root->left,ans);
         ans.push_back(root->val);
         recursive(root->right,ans);
-
     }
 
-    vector<int> morris(TreeNode* root){
-        TreeNode* curr = root;
-        vector<int> ans;
-        if(root == nullptr){
-            return ans;
-        }
 
-        while(curr!=nullptr){
-            if(curr->left==nullptr){
-                ans.push_back(curr->val);
-                curr = curr->right;
-            }else{
-                TreeNode* temp = curr->left;
-                while(temp->right!=nullptr && temp->right!=curr){
-                    temp = temp->right;
-                }
-
-                if(temp->right == nullptr){
-                    temp->right = curr;
-                    curr = curr->left;
-                }else{
-                    temp->right = nullptr;
-                    ans.push_back(curr->val);
-                    curr = curr->right;
-
-
-
-                }
-            }
-        }
-        return ans;
-
-    }
     vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
 
-        // vector<int> ans;
-        // recursive(root,ans);
-        // return ans;
-
-        // return iterative(root);
-
-        return morris(root);
-
+        recursive(root,ans);
         
+
+        return ans;
     }
 };
