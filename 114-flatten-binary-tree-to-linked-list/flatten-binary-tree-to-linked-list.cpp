9776075c11bcb0b1c7c9cacf8da1b66ce15f059c
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-17 19:00:14 +0530

    Time: 10 ms (11.56%) | Memory: 15.7 MB (58.19%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -11,65 +11,32 @@
  */
 class Solution {
 public:
-
-    void preorder(TreeNode* root,vector<int> &ans){
-        if(root==nullptr){
-            return;
-        }
-
-        ans.push_back(root->val);
-        preorder(root->left,ans);
-        preorder(root->right,ans);
-
-    }
-
-    void brute(TreeNode* root){
-        vector<int> ans;
-        preorder(root,ans);
-
-        TreeNode* curr = root;
-
-        for(int i=1; i<ans.size(); i++){
-
-            // if(curr!=nullptr){
-                curr->left = nullptr;
-                curr->right = new TreeNode(ans[i]);
-                curr = curr->right;
-            // }
-            
-        }
-
-        // return root;
-    }
-
-    void optimal(TreeNode* root){
-
-        TreeNode* curr = root;
-
-        while(curr!=nullptr){
-            if(curr->left!=nullptr){
-                TreeNode* temp = curr->left;
-                while(temp->right!=nullptr){
-                    temp = temp->right;
-                }
-
-                temp->right = curr->right;
-                curr->right = curr->left;
-                curr->left = nullptr;
-            }
-
-            curr = curr->right;
-        }
-
-
-    }
     void flatten(TreeNode* root) {
 
-        // brute(root);
+        TreeNode* curr = root;
+        while(curr!=nullptr) {
+            if(curr->left!=nullptr) {
 
-        optimal(root);
+                TreeNode* right = curr->right;
 
-        
+                TreeNode* left = curr->left;
+
+                curr->right = left;
+                TreeNode* left1 = left;
+                while(left->right!=nullptr) {
+                    left = left->right;
+                }
+
+                left->right = right;
+                curr->left = nullptr;
+
+                curr = left1;
+
+                
+            }else{
+                curr = curr->right;
+            }
+        }
         
     }
 };
