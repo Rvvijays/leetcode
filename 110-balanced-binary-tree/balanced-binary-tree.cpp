01cba69e3eec504e36b16669fdef2cbf43c3214a
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-17 15:19:32 +0530

    Time: 12 ms (40.24%) | Memory: 21.5 MB (77.03%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -12,71 +12,33 @@
 class Solution {
 public:
 
-    int height(TreeNode* root){
-        if(root == nullptr){
+    int height(TreeNode* root) {
+        if(root==nullptr) {
             return 0;
         }
 
         int left = height(root->left);
         int right = height(root->right);
 
-        return 1 + max(left,right);
+        return max(left,right) + 1;
     }
 
-    bool check(TreeNode* root){
-        if(root==nullptr){
+
+    bool isBalanced(TreeNode* root) {
+
+        if(root==nullptr) {
             return true;
         }
 
         int left = height(root->left);
         int right = height(root->right);
 
-        // cout<<"root:"<<root->val<<" left:"<<left<<" right:"<<right<<endl;
-
-
-
-        if(abs(left - right) <= 1){
-           bool leftCehck =  check(root->left);
-           bool rightCeck = check(root->right);
-
-           return leftCehck && rightCeck;
-        }
-
-        return false;
-
-
-
-    }
-
-    int postOrder(TreeNode* root){
-        if(root == nullptr){
-            return 0;
-        }
-
-        int left = postOrder(root->left);
-        int right = postOrder(root->right);
-
-        if(left == -1 || right == -1){
-            return -1;
-        }
-
-        if(abs(left-right)<=1){
-            return 1 + max(left,right);
-        }
-
-        return -1;
-
-    }
-    bool isBalanced(TreeNode* root) {
-
-        // return check(root);
-
-        int tt = postOrder(root);
-        if(tt == -1){
+        if(abs(left-right) > 1) {
             return false;
         }
 
-        return true;
+        return isBalanced(root->left) && isBalanced(root->right);
+        
         
     }
 };
