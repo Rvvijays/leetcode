c7baf6a0654388abadc6bc38818f0b9686689072
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-17 15:08:02 +0530

    Time: 8 ms (51.63%) | Memory: 18.4 MB (7.10%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -11,54 +11,53 @@
  */
 class Solution {
 public:
+
     int widthOfBinaryTree(TreeNode* root) {
 
-        if(root== nullptr){
-            return 0;
-        }
+        queue<pair<TreeNode*,int>> q;
 
-        queue<pair<TreeNode*,long long>> q;
         q.push({root,0});
 
         int width = 0;
 
-        while(!q.empty()){
-            
+        while(!q.empty()) {
+
             int size = q.size();
 
-            int startIndex = 0;
-            int lastIndex = 0;
-            int currMin = q.front().second;
+            int first,last;
 
+            int minid = q.front().second;
 
-            for(int i=0; i<size; i++){
-                auto p = q.front();
+            for(int i=0; i<size; i++) {
+
+                auto it = q.front();
                 q.pop();
 
-                TreeNode* node = p.first;
-                long long index = p.second - currMin;
+                TreeNode* node = it.first;
+                long id = it.second - minid;
+
                 if(i==0){
-                    startIndex = index;
-                }
-                lastIndex = index;
-                
-
-                if(node->left!=nullptr){
-                    q.push({node->left,2*index});
+                    first = id;
                 }
 
-                if(node->right!=nullptr){
-                    q.push({node->right,2*index+1});
+                if(i == size - 1){
+                    last = id;
+                }
+
+                if(node->left!=nullptr) {
+                    q.push({node->left,2*id});
+                }
+
+                if(node->right!=nullptr) {
+                    q.push({node->right,2*id+1});
                 }
             }
 
-            
-            width = max(width,lastIndex-startIndex +1);
-       
-
+            width = max(width, last-first+1);
         }
 
         return width;
-
+        
+        
     }
 };
