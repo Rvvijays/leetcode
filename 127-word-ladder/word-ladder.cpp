ee353d96891d0e80078f26297b1a724036d2316b
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-19 22:42:02 +0530

    Time: 2913 ms (5.01%) | Memory: 18.9 MB (91.91%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,46 +1,71 @@
 class Solution {
 public:
+
+    int isValid(string s1, string s2) {
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+
+        int diff = 0;
+        for(int i=0; i<s1.length(); i++) {
+            if(s1[i]!=s2[i]){
+                diff++;
+            }
+
+            if(diff > 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
 
-        unordered_map<string,bool> words;
-
-        for(auto it: wordList) {
-            words[it] = true;
-        }
-
+    //    unordered_map<string,bool> words;
+        
+    //     for(auto word: wordList){
+    //         words[word] = true;
+    //     }
+        
+        
         queue<pair<string,int>> q;
-
+        unordered_map<string,bool> vis;
+        
         q.push({beginWord,1});
-
+        
         while(!q.empty()){
-            string word = q.front().first;
-            int step = q.front().second;
-
-            if(word == endWord){
-                return step;
-            }
+            auto it = q.front();
             q.pop();
-
-            for(int i=0; i<word.size(); i++){
-                string temp = word;
-                for(char ch = 'a'; ch<='z'; ch++){
-                    temp[i] = ch;
-
-                    if(words[temp]==true){
-                        q.push({temp,step+1});
-                        words[temp] = false;
-                    }
-
-                }
-
+            
+            string word = it.first;
+            int level = it.second;
+            
+            if(word == endWord){
+                return level;
             }
             
+            for(int i=0; i<wordList.size(); i++){
 
+                if(vis.find(wordList[i]) == vis.end() && isValid(word,wordList[i])) {
+                    cout<<"checking ..";
+                    q.push({wordList[i],level+1});
+                    vis[wordList[i]] = true;
+                }
+                
+                // char temp = word[i];
+                // for(char c = 'a'; c<='z'; c++){
+                //     word[i] = c;
+                //     if(words[word]){
+                       
+                //         words[word] = false;
+                //     }
+                // }
+                // word[i] = temp;
+            }
+            
         }
-
-        return 0;
-
         
+        return 0;
         
     }
 };
