b8727a42bf94382111baf1248e607ffac665845c
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-23 00:28:51 +0530

    Time: 170 ms (26.04%) | Memory: 11.8 MB (95.70%) - LeetSync

---
+++ 277-find-the-celebrity/find-the-celebrity.cpp
@@ -0,0 +1,28 @@
+/* The knows API is defined for you.
+      bool knows(int a, int b); */
+
+class Solution {
+public:
+    bool isCelebrity(int i, int n) {
+        for (int j = 0; j < n; j++) {
+            if(i==j)continue;
+            if(knows(i,j) || !knows(j,i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    int findCelebrity(int n) {
+        for(int i=0; i<n; i++) {
+            
+            if(isCelebrity(i,n)) {
+                return i;
+            }
+        }
+
+        return -1;
+        
+    }
+
+
+};
