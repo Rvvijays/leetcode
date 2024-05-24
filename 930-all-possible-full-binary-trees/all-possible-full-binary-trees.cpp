8835b401d27e1b5d1e4eebb302931904dbf6105a
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-05-25 03:41:38 +0530

    Time: 60 ms (40.48%) | Memory: 36.1 MB (19.31%) - LeetSync

---
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<TreeNode*> allPossibleFBT(int n) {
+        vector<TreeNode*> ans;
+
+        if(n%2 == 0){
+            return ans;
+        }
+
+        if(n == 1) {
+            ans.push_back(new TreeNode(0));
+            return ans;
+        }
+
+        for(int i=1; i<n; i+=2) {
+
+            vector<TreeNode*> left = allPossibleFBT(i);
+            vector<TreeNode*> right = allPossibleFBT(n-i-1);
+
+            for(auto nodel : left) {
+                for(auto noder : right) {
+                    TreeNode* root = new TreeNode(0);
+                    root->left = nodel;
+                    root->right = noder;
+                    ans.push_back(root);
+                }
+            }
+        }
+
+        return ans;
+
+        
+        
+        
+    }
+};
