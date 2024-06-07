b6fce430c746028db02c6fffe2d47f18c5a7e2fa
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-06-08 02:28:39 +0530

    Time: 65 ms (89.24%) | Memory: 76.5 MB (50.47%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -1,5 +1,95 @@
+class TrieNode {
+    char ch;
+    TrieNode* children[27];
+    bool end;
+
+    public:
+    TrieNode() {
+        end = false;
+        for(int i=0; i<26; i++) {
+            children[i] = nullptr;
+        }
+    }
+
+    void add(char ch) {
+        children[ch-'a'] = new TrieNode();
+    }
+
+    bool contains(char ch) {
+        return children[ch-'a']!=nullptr;
+    }
+
+    TrieNode* get(char ch) {
+        return children[ch-'a'];
+    }
+
+    bool isEnd() {
+        return end;
+    }
+
+    void setEnd() {
+        end =true;
+    }
+
+};
+
+
+
 class Solution {
+
+    TrieNode* root;
+
+
+
 public:
+
+    void insert(string word) {
+        TrieNode* temp = root;
+
+        for(int i=0; i<word.size(); i++) {
+            if(!temp->contains(word[i])) {
+                temp->add(word[i]);
+            }
+
+            temp = temp->get(word[i]);
+        }
+
+        temp->setEnd();
+    }
+
+    bool startsWith(string word) {
+        TrieNode* temp = root;
+
+        for(int i=0; i<word.size(); i++) {
+            if(!temp->contains(word[i])) {
+                return false;
+            }
+
+            temp = temp->get(word[i]);
+        }
+
+        return true;
+    }
+
+    string search(string word) {
+        TrieNode* temp = root;
+
+        for(int i=0; i<word.size(); i++) {
+            if(!temp->contains(word[i])) {
+                return word;
+            }
+
+            temp = temp->get(word[i]);
+
+            if(temp->isEnd()) {
+                return word.substr(0,i+1);
+            }
+        }
+
+        return word;
+    }
+
+                     
     string brute(vector<string>& dictionary, string sentence) {
         unordered_map<string, bool> hash;
 
@@ -41,7 +131,34 @@ public:
     }
     string replaceWords(vector<string>& dictionary, string sentence) {
 
-        return brute(dictionary, sentence);
+        root = new TrieNode();
+
+        // return brute(dictionary, sentence);
+
+        for (int i=0; i<dictionary.size(); i++) {
+            insert(dictionary[i]);
+        }
+
+
+        stringstream s(sentence);
+
+        string str;
+
+        string ans;
+
+        while(getline(s,str,' ')) {
+
+            ans += search(str) + " ";
+
+        }
+
+        ans.pop_back();
+
+        return ans;
+
+
+
+
 
 
 
