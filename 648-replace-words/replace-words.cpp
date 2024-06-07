7909e5a2b8919098d03199962efcbec05ac1c0cb
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-06-08 00:06:02 +0530

    Time: 209 ms (32.32%) | Memory: 56 MB (81.86%) - LeetSync

--- 648-replace-words/replace-words.cpp
+++ 648-replace-words/replace-words.cpp
@@ -1,38 +1,49 @@
 class Solution {
 public:
-    string replaceWords(vector<string>& dictionary, string sentence) {
-        map<string,bool> hash;
+    string brute(vector<string>& dictionary, string sentence) {
+        unordered_map<string, bool> hash;
 
-        for(int i=0; i<dictionary.size(); i++) {
+        for (int i = 0; i < dictionary.size(); i++) {
             hash[dictionary[i]] = true;
         }
 
+        // sc: o(n)
+        // tc: o(nlogn)
+
         string ans;
-        
+
         stringstream s(sentence);
         string temp;
 
-        while(getline(s,temp,' ')) {
+        while (getline(s, temp, ' ')) {
             string tt = "";
             bool found = false;
-            for(int i=0; i<temp.size(); i++) {
+            for (int i = 0; i < temp.size(); i++) {
                 tt += temp[i];
 
-                if(hash.find(tt)!= hash.end()) {
+                if (hash.find(tt) != hash.end()) {
                     ans += tt + " ";
                     found = true;
                     break;
                 }
             }
 
-            if(!found){
-            ans += tt + " ";
-
+            if (!found) {
+                ans += tt + " ";
             }
         }
+
+        // tc : o(nm)
 
         ans.pop_back();
 
         return ans;
+    }
+    string replaceWords(vector<string>& dictionary, string sentence) {
+
+        return brute(dictionary, sentence);
+
+
+
     }
 };
