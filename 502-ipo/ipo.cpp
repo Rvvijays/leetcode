6456703b9ebbbc14d105092d350979a7bc358a3a
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-06-19 12:39:04 +0530

    Time: 152 ms (19.96%) | Memory: 81.6 MB (83.70%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,40 +1,52 @@
 class Solution {
 public:
-    int findMaximizedCapital(int k, int w, vector<int>& profits,
-                             vector<int>& capital) {
-        int n = profits.size();
-        std::vector<std::pair<int, int>> projects;
 
-        // Creating vector of pairs (capital, profits)
-        for (int i = 0; i < n; ++i) {
-            projects.emplace_back(capital[i], profits[i]);
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+
+        int n = profits.size();
+        vector<pair<int,int>> capProfit(n);
+
+        for (int i = 0; i < n; i++) {
+            capProfit[i] = {capital[i],profits[i]};
         }
 
-        // Sorting projects by capital required
-        std::sort(projects.begin(), projects.end());
+        sort(capProfit.begin(), capProfit.end());
 
-        // Max-heap to store profits, using greater to create a max-heap
-        std::priority_queue<int> maxHeap;
-        int i = 0;
 
-        // Main loop to select up to k projects
-        for (int j = 0; j < k; ++j) {
-            // Add all profitable projects that we can afford
-            while (i < n && projects[i].first <= w) {
-                maxHeap.push(projects[i].second);
+        priority_queue<int> pq;
+
+
+        // int ans = 0;
+
+        int i=0;
+        while (k>0) {
+            // cout<<"capital: "<<w<<endl;
+            while(i < n && capProfit[i].first <= w) {
+                // cout<<"capital: "<<w<<endl;
+                // cout<<" added: "<<capProfit[i].second<<endl;
+                pq.push(capProfit[i].second);
+                // cout<<"index: "<<i<<" push: "<<capProfit[i].second<<endl;
                 i++;
             }
 
-            // If no projects can be funded, break out of the loop
-            if (maxHeap.empty()) {
+            if(pq.empty()) {
                 break;
             }
 
-            // Otherwise, take the project with the maximum profit
-            w += maxHeap.top();
-            maxHeap.pop();
+            if(!pq.empty()) {
+                // ans += pq.top();
+                // cout<<"added to anser:"<<pq.top()<<endl;
+                w += pq.top();
+                // cout<<"pop: "<<pq.top()<<endl;
+                pq.pop();
+                k--;
+            }
+
+            
         }
 
         return w;
+    
+
     }
 };
