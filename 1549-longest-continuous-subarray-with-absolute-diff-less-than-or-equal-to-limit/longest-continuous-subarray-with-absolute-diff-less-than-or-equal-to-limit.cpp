5ccf05f76e6d1db046119ac4875c6db056970050
Ranvijay Sharma <ranvijaysharma18@gmail.com>
2024-06-23 19:09:33 +0530

    Time: 147 ms (22.11%) | Memory: 81 MB (30.80%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int longestSubarray(vector<int>& nums, int limit) {
 
+    int brute(vector<int>& nums, int limit) {
         int n = nums.size();
 
         int i = 0;
@@ -43,6 +43,36 @@ public:
 
 
         return maxLen;
+    }
+    int longestSubarray(vector<int>& nums, int limit) {
+
+        int n = nums.size();
+
+
+        multiset<int> st;
+        int i =0;
+        int j = 0;
+
+        int maxLen = 0;
+
+        while( j < n) {
+            st.insert(nums[j]);
+
+            while(*st.rbegin() - *st.begin() > limit) {
+                st.erase(st.find(nums[i]));
+                i++;
+            }
+
+            maxLen = max(maxLen, j - i + 1);
+
+            j++;
+
+
+        }
+
+        return maxLen;
+
+       
 
 
     }
